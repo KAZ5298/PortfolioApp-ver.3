c6bf1c285504d738707d747bbf333f85087cb9eb
EC2 Default User <ec2-user@ip-172-31-39-224.ap-northeast-1.compute.internal>
2021-04-22 21:28:27 +0000

    ユーザー一覧画面作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   resource :sessions, only: [:new, :create, :destroy]
   resource :registrations, only: [:new, :create]
+  resources :users, only: [:index, :show]
   root to: 'registrations#new'
 end
--- db/schema.rb
+++ db/schema.rb
@@ -15,7 +15,6 @@ ActiveRecord::Schema.define(version: 2021_04_22_194347) do
   create_table "users", force: :cascade do |t|
     t.string "email", null: false
     t.string "password_digest", null: false
-    t.string "salt", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.string "name"
