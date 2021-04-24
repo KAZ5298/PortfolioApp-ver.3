5c9e093755122b061fa39b73bb646dca97a7059a
EC2 Default User <ec2-user@ip-172-31-39-224.ap-northeast-1.compute.internal>
2021-04-24 06:44:46 +0000

    ツイート機能修正

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -31,10 +31,9 @@ class TweetsController < ApplicationController
   def create
     @tweet = Tweet.new(tweet_params)
     @tweet.user = current_user
-
     respond_to do |format|
       if @tweet.save
-        format.html { redirect_to tweets_url, notice: 'Tweet was successfully created.' }
+        format.html { redirect_to tweets_url, notice: 'ツイートを送信しました' }
         format.json { render :show, status: :created, location: @tweet }
       else
         @tweets = Tweet.all
@@ -48,7 +47,7 @@ class TweetsController < ApplicationController
   def update
     respond_to do |format|
       if @tweet.update(tweet_params)
-        format.html { redirect_to @tweet, notice: "Tweet was successfully updated." }
+        format.html { redirect_to @tweet, notice: "ツイートを更新しました" }
         format.json { render :show, status: :ok, location: @tweet }
       else
         format.html { render :edit, status: :unprocessable_entity }
@@ -61,7 +60,7 @@ class TweetsController < ApplicationController
   def destroy
     @tweet.destroy
     respond_to do |format|
-      format.html { redirect_to tweets_url, notice: "Tweet was successfully destroyed." }
+      format.html { redirect_to tweets_url, notice: "ツイートを削除しました" }
       format.json { head :no_content }
     end
   end
