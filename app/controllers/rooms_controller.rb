5ff571299d3859ced14d72566236d7c6298e06df
EC2 Default User <ec2-user@ip-172-31-39-224.ap-northeast-1.compute.internal>
2021-05-04 11:35:50 +0000

    DM機能レイアウト変更

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -3,8 +3,8 @@ class RoomsController < ApplicationController
   
   def create
     @room = Room.create
-    @entry1 = Entry.create(room_id: @room.id, user_id: current_user.id)
-    @entry2 = Entry.create(params.require(:entry).permit(:user_id, :room_id).merge(room_id: @room.id))
+    Entry.create(room_id: @room.id, user_id: current_user.id)
+    Entry.create(params.require(:entry).permit(:user_id, :room_id).merge(room_id: @room.id))
     redirect_to "/rooms/#{@room.id}"
   end
 
